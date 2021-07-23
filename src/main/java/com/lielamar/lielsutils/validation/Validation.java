c811e67447ffbab20f1243c5006584d0c29d30a5
David <darknight4123@gmail.com>
2021-07-23 04:24:56 +0300

    Added PredicateUtils to statically import not(Predicate) from

--- src/main/java/com/lielamar/lielsutils/validation/Validation.java
+++ src/main/java/com/lielamar/lielsutils/validation/Validation.java
@@ -1,10 +1,10 @@
 package com.lielamar.lielsutils.validation;
 
+import static com.lielamar.lielsutils.PredicateUtils.not;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
 public interface Validation<T> {
 
@@ -12,23 +12,19 @@ public interface Validation<T> {
     String getMessage();
     boolean validate();
 
-    static <T> Predicate<T> not(Predicate<T> t) {
-        return t.negate();
-    }
-
     /**
      * Returns a list of violations (validations that failed)
      *
      * @param parameters   List of validations to check
      * @return             List of violations
      */
-    static List<Validation<?>> validateParameters(List<Validation<?>> parameters) {
+    public static List<Validation<?>> validateParameters(List<Validation<?>> parameters) {
     	return parameters.stream()
     			.filter(not(Validation::validate))
     			.collect(toList());
     }
-
-	static List<Validation<?>> validateParameters(Validation<?>... parameters) {
+    
+    public static List<Validation<?>> validateParameters(Validation<?>... parameters) {
     	List<Validation<?>> list = Arrays.asList(parameters);
     	return validateParameters(list);
     }
