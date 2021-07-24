1212ac976bf349fa18a3058357dc428764d4d1d7
David <darknight4123@gmail.com>
2021-07-25 01:30:53 +0300

    Completely refactored the Validations package
    Validations are now created through ValidationFactory

--- src/main/java/com/lielamar/lielsutils/validation/AbstractValidation.java
+++ src/main/java/com/lielamar/lielsutils/validation/AbstractValidation.java
@@ -1,43 +1,15 @@
 package com.lielamar.lielsutils.validation;
 
-import java.util.function.Predicate;
-
 public abstract class AbstractValidation<T> implements Validation<T> {
 	
-	protected final T value;
-    private final String message;
-    protected final Predicate<T> predicate;
+    private final String errorMessage;
     
-    protected AbstractValidation(T value) {
-        this(value, "", null);
-    }
-
-    protected AbstractValidation(T value, String message) {
-        this(value, message, null);
-    }
-
-    protected AbstractValidation(T value, Predicate<T> predicate) {
-        this(value, "", predicate);
-    }
-
-    protected AbstractValidation(T value, String message, Predicate<T> predicate) {
-        this.value = value;
-        this.message = message;
-        this.predicate = predicate;
+    protected AbstractValidation(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
     
 	@Override
-	public T getValue() {
-        return this.value;
+	public String getErrorMessage(T filteredObject) {
+		return String.format(this.errorMessage, filteredObject);
 	}
-
-	@Override
-	public String getMessage() {
-		return this.message;
-	}
-	
-	@Override
-	public boolean validate() {
-        return this.predicate == null || this.predicate.test(this.value);
-    }
 }
--- src/main/java/com/lielamar/lielsutils/validation/Validation.java
+++ src/main/java/com/lielamar/lielsutils/validation/Validation.java
@@ -1,31 +1,8 @@
 package com.lielamar.lielsutils.validation;
 
-import static com.lielamar.lielsutils.PredicateUtils.not;
-import static java.util.stream.Collectors.toList;
+import java.util.function.Predicate;
 
-import java.util.Arrays;
-import java.util.List;
+public interface Validation<T> extends Predicate<T> {
 
-public interface Validation<T> {
-
-    T getValue();
-    String getMessage();
-    boolean validate();
-
-    /**
-     * Returns a list of violations (validations that failed)
-     *
-     * @param parameters   List of validations to check
-     * @return             List of violations
-     */
-    public static List<Validation<?>> validateParameters(List<Validation<?>> parameters) {
-    	return parameters.stream()
-    			.filter(not(Validation::validate))
-    			.collect(toList());
-    }
-    
-    public static List<Validation<?>> validateParameters(Validation<?>... parameters) {
-    	List<Validation<?>> list = Arrays.asList(parameters);
-    	return validateParameters(list);
-    }
+	String getErrorMessage(T filteredObject);
 }
