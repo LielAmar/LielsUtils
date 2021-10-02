8f998e72492dcf146a0e107934fa7d7a41fd042c
Liel <lialiko3@gmail.com>
2021-10-02 20:23:10 +0300

    Made HashUtils static

--- src/main/java/com/lielamar/lielsutils/hash/HashUtils.java
+++ src/main/java/com/lielamar/lielsutils/hash/HashUtils.java
@@ -7,7 +7,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class HashUtils {
 
-    public String hashSHA256(String plain) throws NoSuchAlgorithmException { return Hashing.sha256().hashString(plain, StandardCharsets.UTF_8).toString(); }
-    public String hashSHA384(String plain) throws NoSuchAlgorithmException { return Hashing.sha384().hashString(plain, StandardCharsets.UTF_8).toString(); }
-    public String hashSHA512(String plain) throws NoSuchAlgorithmException { return Hashing.sha512().hashString(plain, StandardCharsets.UTF_8).toString(); }
+    public static String hashSHA256(String plain) throws NoSuchAlgorithmException { return Hashing.sha256().hashString(plain, StandardCharsets.UTF_8).toString(); }
+    public static String hashSHA384(String plain) throws NoSuchAlgorithmException { return Hashing.sha384().hashString(plain, StandardCharsets.UTF_8).toString(); }
+    public static String hashSHA512(String plain) throws NoSuchAlgorithmException { return Hashing.sha512().hashString(plain, StandardCharsets.UTF_8).toString(); }
 }
