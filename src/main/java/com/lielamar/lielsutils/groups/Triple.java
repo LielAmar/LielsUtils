94645e5e2cd1d5039e3c671f067bb19d84b72db9
David <darknight4123@gmail.com>
2021-10-08 01:14:11 +0300

    Triple now extends Pair, in order to save code

--- src/main/java/com/lielamar/lielsutils/groups/Triple.java
+++ src/main/java/com/lielamar/lielsutils/groups/Triple.java
@@ -1,22 +1,13 @@
 package com.lielamar.lielsutils.groups;
 
-public class Triple<A, B, C> {
+public class Triple<A, B, C> extends Pair<A, B> {
 
-    private A a;
-    private B b;
     private C c;
 
     public Triple(A a, B b, C c) {
-        this.a = a;
-        this.b = b;
+    	super(a, b);
         this.c = c;
     }
-
-    public A getA() { return a; }
-    public void setA(A a) { this.a = a; }
-
-    public B getB() { return b; }
-    public void setB(B b) { this.b = b; }
 
     public C getC() { return c; }
     public void setC(C c) { this.c = c; }
