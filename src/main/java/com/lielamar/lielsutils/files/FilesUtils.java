db1158916744a3eea2ea8eb6f5fedc8677b0cea9
Liel <lialiko3@gmail.com>
2021-10-21 00:57:10 +0300

    Updated FilesUtils to support parsing to SerializableDocument

--- src/main/java/com/lielamar/lielsutils/files/FilesUtils.java
+++ src/main/java/com/lielamar/lielsutils/files/FilesUtils.java
@@ -1,5 +1,7 @@
 package com.lielamar.lielsutils.files;
 
+import com.google.gson.JsonSyntaxException;
+import com.lielamar.connections.serializable.SerializableDocument;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
@@ -16,7 +18,7 @@ public class FilesUtils {
      * @return               Parsed file data as String
      * @throws IOException   Throws an IOException if an IO operation failed
      */
-    public static String parseFileIntoString(@NotNull File file) throws IOException {
+    public static @NotNull String parseFileIntoString(@NotNull File file) throws IOException {
         BufferedReader fileReader = new BufferedReader(new FileReader(file));
 
         StringBuilder fileData = new StringBuilder();
@@ -31,5 +33,16 @@ public class FilesUtils {
         fileReader.close();
 
         return fileData.toString();
+    }
+
+    /**
+     * Parses the content of the given file into a SerializableDocument
+     *
+     * @param file                   File to parse
+     * @return                       Parsed file data as SerializableDocument
+     * @throws JsonSyntaxException   Throws an JsonSyntaxException if the content of the file is not a valid json
+     */
+    public static @NotNull SerializableDocument parseFileIntoSerializableDocument(@NotNull File file) throws JsonSyntaxException {
+        return new SerializableDocument(parseFileIntoSerializableDocument(file));
     }
 }
